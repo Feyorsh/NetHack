1493c815d962f0f8f750ab07e04bad11c84f1fd4
George Huebner <george@feyor.sh>
2025-08-26 19:33:18 -0500

    lisp_select_menu: sanity check input and retry

--- win/lisp/winlisp.c
+++ win/lisp/winlisp.c
@@ -1101,14 +1101,12 @@ lisp_select_menu(winid window, int how, menu_item **menu_list)
   char *token;
   int size = 0;
 
+redo:
   lisp_cmd ("select-menu",
 	    lisp_int (window);
 	    lisp_literal (how_to_string (how)));
 
   read_string ("menu", &list);
-
-/*   lisp_prompt ("menu"); */
-/*   fgets (list, LINESIZ, stdin); */
 
   /* The client should submit a structure like this:
 
@@ -1141,7 +1139,11 @@ lisp_select_menu(winid window, int how, menu_item **menu_list)
 	}
 
       /* assign the item ID */
-      lisp_get_menu_identifier (atoi (token), &(*menu_list)[size-1].item );
+      if (!lisp_get_menu_identifier (atoi (token), &(*menu_list)[size-1].item )) {
+          free(*menu_list);
+          free(list);
+          goto redo;
+      }
 
       /* Read the item count */
       token = strtok (NULL, delim);
