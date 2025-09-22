a1b92ac81fdd07d23771cc52af6e39953d7bac3b
George Huebner <george@feyor.sh>
2025-09-22 13:54:11 -0500

    lisp_display_file: send file to clients that can't display it themselves

--- win/lisp/winlisp.c
+++ win/lisp/winlisp.c
@@ -1695,9 +1695,33 @@ lisp_display_file(str, complain)
      const char *str;
      boolean complain;
 {
+  char *success;
   lisp_cmd ("display-file",
 	    lisp_string (str);
 	    complain ? lisp_t : lisp_nil);;
+
+  read_string("string", &success);
+  if (strcmp(success, "t")) {
+      char buf[BUFSZ];
+      dlb *fp = dlb_fopen(str, "r");
+
+      if (fp == NULL) {
+          if (complain) {
+              Sprintf(buf, "cannot open file %s", str);
+              lisp_cmd ("receive-file", lisp_string (buf); lisp_string("error"););;
+          }
+          return;
+      }
+
+      while (dlb_fgets(buf, BUFSZ, fp) != NULL) {
+          lisp_cmd ("receive-file", lisp_string (buf););;
+      }
+
+      dlb_fclose(fp);
+      lisp_cmd ("receive-file", lisp_string (""); lisp_t;);;
+  }
+
+  free(success);
 }
 
 char
