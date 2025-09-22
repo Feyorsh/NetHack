ed6c3b979fbbcc9161772aa1a17a881967e76d3f
George Huebner <george@feyor.sh>
2025-09-22 16:25:13 -0500

    send config file to client on startup

--- win/lisp/winlisp.c
+++ win/lisp/winlisp.c
@@ -1537,7 +1537,32 @@ lisp_number_pad(int state UNUSED)
 void
 lisp_init_nhwindows(int *argcp, char **argv)
 {
+  char *need_options_file_p;
   int i;
+
+  lisp_cmd ("need-options-file",);
+  read_string("string", &need_options_file_p);
+
+  if (!strcmp(need_options_file_p, "t")) {
+      char buf[BUFSZ];
+      FILE *fp = fopen(get_configfile(), "r");
+
+      if (fp == NULL) {
+          Sprintf(buf, "cannot open options file %s", get_configfile());
+          lisp_cmd ("receive-file", lisp_string(buf); lisp_string("error"););;
+          return;
+      }
+
+      while (fgets(buf, BUFSZ, fp) != NULL) {
+          lisp_cmd ("receive-file", lisp_string (buf););;
+      }
+
+      fclose(fp);
+      lisp_cmd ("receive-file", lisp_string (""); lisp_t;);;
+  }
+
+  free(need_options_file_p);
+
 
   /* Print each command-line option, constructing a list of strings */
   lisp_cmd ("init-nhwindows",
