fe17893c6c4b6c95b36d8d4d902a158736524c18
George Huebner <george@feyor.sh>
2025-10-11 11:52:17 -0500

    fix double free

--- win/lisp/winlisp.c
+++ win/lisp/winlisp.c
@@ -1640,9 +1640,8 @@ lisp_init_nhwindows(argcp,argv)
 
       fclose(fp);
       lisp_cmd ("receive-file", lisp_string (""); lisp_t;);;
+      free(need_options_file_p);
   }
-
-  free(need_options_file_p);
 
 
   /* Print each command-line option, constructing a list of strings */
@@ -1728,28 +1727,28 @@ lisp_display_file(str, complain)
 	    lisp_string (str);
 	    complain ? lisp_t : lisp_nil);;
 
-  read_string("string", &success);
-  if (strcmp(success, "t")) {
-      char buf[BUFSZ];
-      dlb *fp = dlb_fopen(str, "r");
+  if (!read_string("string", &success)) {
+      if (strcmp(success, "t")) {
+          char buf[BUFSZ];
+          dlb *fp = dlb_fopen(str, "r");
 
-      if (fp == NULL) {
-          if (complain) {
-              Sprintf(buf, "cannot open file %s", str);
-              lisp_cmd ("receive-file", lisp_string (buf); lisp_string("error"););;
+          if (fp == NULL) {
+              if (complain) {
+                  Sprintf(buf, "cannot open file %s", str);
+                  lisp_cmd ("receive-file", lisp_string (buf); lisp_string("error"););;
+              }
+              return;
           }
-          return;
-      }
 
-      while (dlb_fgets(buf, BUFSZ, fp) != NULL) {
-          lisp_cmd ("receive-file", lisp_string (buf););;
-      }
+          while (dlb_fgets(buf, BUFSZ, fp) != NULL) {
+              lisp_cmd ("receive-file", lisp_string (buf););;
+          }
 
-      dlb_fclose(fp);
-      lisp_cmd ("receive-file", lisp_string (""); lisp_t;);;
+          dlb_fclose(fp);
+          lisp_cmd ("receive-file", lisp_string (""); lisp_t;);;
+      }
+      free(success);
   }
-
-  free(success);
 }
 
 char
