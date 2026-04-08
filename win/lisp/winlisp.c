39b9064288f94ed7255410322c05b88845cbc783
George Huebner <george@feyor.sh>
2026-04-08 00:16:36 -0500

    read_command: fix sscanf overflow

--- win/lisp/winlisp.c
+++ win/lisp/winlisp.c
@@ -492,7 +492,7 @@ char *count;
     *count = 0;
     if (read_string(prompt, &buf) == -1)
         return -1;
-    rv = sscanf(buf, "%s %s", cmd, count);
+    rv = sscanf(buf, "%255s %1023s", cmd, count);
     free(buf);
     if (rv != 2)
         *count = 0;
